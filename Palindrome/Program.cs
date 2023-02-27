d01b4304f4d1b92ac0321f08985226e1d004c016
Ahmed Allam <Ahmed.Allam@esl.uk.com>
2023-02-27 16:15:43 +0200

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Palindrome;
+
+Console.WriteLine("Palindrome, Maybe!");
+Worker worker = new Worker();
+Console.ReadKey();
---
+++ Palindrome/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Worker
+    {
+        public Worker()
+        {
+            Work();
+        }
+        public void Work()
+        {
+            Console.Write("Number: ");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsPalindrome(input) ? "Palindrome" : "Not Palindrome");
+        }
+        public bool IsPalindrome(int number)
+        {
+            string numberToString = number.ToString();
+            string numberReversed = string.Join("", numberToString.Reverse<char>().ToArray());
+            return (numberReversed.Equals(numberToString) ? true : false);
+        }
+    }
+}
